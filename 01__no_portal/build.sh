150444b9cdb5c1a8ba4a3d050332cf3d4ee4cb20
Petter Holmström <petter@vaadin.com>
2023-09-12 15:11:45 +0300

    Remove unneeded build calls

--- 01__no_portal/build.sh
+++ 01__no_portal/build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 docker build -t hillamicro/noportal/portal-shell portal-shell/
 docker compose build
-../mvnw -f portal-shell/pom.xml clean install
-../mvnw -f portal-gateway/pom.xml clean package
-../mvnw -f microfrontend-a/pom.xml clean package
-../mvnw -f microfrontend-b/pom.xml clean package
